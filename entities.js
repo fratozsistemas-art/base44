24dcca54030241241f1258169e655dc95719a8b9
fratozsistemas-art <fratozsistemas@gmail.com>
2025-10-31 19:42:46 +0000

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,71 @@
+import { base44 } from './base44Client';
+
+
+export const Strategy = base44.entities.Strategy;
+
+export const Analysis = base44.entities.Analysis;
+
+export const QuickAction = base44.entities.QuickAction;
+
+export const FileAnalysis = base44.entities.FileAnalysis;
+
+export const Workspace = base44.entities.Workspace;
+
+export const TechStackAnalysis = base44.entities.TechStackAnalysis;
+
+export const SIUAnalysis = base44.entities.SIUAnalysis;
+
+export const KnowledgeItem = base44.entities.KnowledgeItem;
+
+export const MonitoringAlert = base44.entities.MonitoringAlert;
+
+export const Notification = base44.entities.Notification;
+
+export const Subscription = base44.entities.Subscription;
+
+export const KnowledgeGraphNode = base44.entities.KnowledgeGraphNode;
+
+export const KnowledgeGraphRelationship = base44.entities.KnowledgeGraphRelationship;
+
+export const GraphCompany = base44.entities.GraphCompany;
+
+export const GraphExecutive = base44.entities.GraphExecutive;
+
+export const GraphTechnology = base44.entities.GraphTechnology;
+
+export const GraphFramework = base44.entities.GraphFramework;
+
+export const GraphMetric = base44.entities.GraphMetric;
+
+export const GraphInvestor = base44.entities.GraphInvestor;
+
+export const GraphMarket = base44.entities.GraphMarket;
+
+export const FundingSource = base44.entities.FundingSource;
+
+export const TSIProject = base44.entities.TSIProject;
+
+export const MonitoringConfig = base44.entities.MonitoringConfig;
+
+export const Alert = base44.entities.Alert;
+
+export const CommunicationAnalysis = base44.entities.CommunicationAnalysis;
+
+export const ESGMetrics = base44.entities.ESGMetrics;
+
+export const ConsistencyScore = base44.entities.ConsistencyScore;
+
+export const TSIDeliverable = base44.entities.TSIDeliverable;
+
+export const KnowledgeSource = base44.entities.KnowledgeSource;
+
+export const Role = base44.entities.Role;
+
+export const UserRole = base44.entities.UserRole;
+
+export const EntityAccess = base44.entities.EntityAccess;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ functions.js
@@ -0,0 +1,71 @@
+import { base44 } from './base44Client';
+
+
+export const createCheckout = base44.functions.createCheckout;
+
+export const stripeWebhook = base44.functions.stripeWebhook;
+
+export const orchestrateSIU = base44.functions.orchestrateSIU;
+
+export const buildKnowledgeGraph = base44.functions.buildKnowledgeGraph;
+
+export const queryKnowledgeGraph = base44.functions.queryKnowledgeGraph;
+
+export const gensparkInspired = base44.functions.gensparkInspired;
+
+export const enforceStopGates = base44.functions.enforceStopGates;
+
+export const auditModuleBoundaries = base44.functions.auditModuleBoundaries;
+
+export const scheduleModuleAudit = base44.functions.scheduleModuleAudit;
+
+export const scheduleKnowledgeGraphSync = base44.functions.scheduleKnowledgeGraphSync;
+
+export const seedKnowledgeGraph = base44.functions.seedKnowledgeGraph;
+
+export const orchestrateTSI = base44.functions.orchestrateTSI;
+
+export const testMonitoringArchitecture = base44.functions.testMonitoringArchitecture;
+
+export const autoNameConversation = base44.functions.autoNameConversation;
+
+export const detectStakeholderContext = base44.functions.detectStakeholderContext;
+
+export const auditQuickActions = base44.functions.auditQuickActions;
+
+export const scheduleMonitoringCheck = base44.functions.scheduleMonitoringCheck;
+
+export const seedKnowledgeGraphRealData = base44.functions.seedKnowledgeGraphRealData;
+
+export const seedIbovespaCompanies = base44.functions.seedIbovespaCompanies;
+
+export const seedKnowledgeGraphComplete = base44.functions.seedKnowledgeGraphComplete;
+
+export const trackTSIProcess = base44.functions.trackTSIProcess;
+
+export const detectParadoxes = base44.functions.detectParadoxes;
+
+export const generateTrustBrokerData = base44.functions.generateTrustBrokerData;
+
+export const syncGAYAContributions = base44.functions.syncGAYAContributions;
+
+export const testMonitoringForCompany = base44.functions.testMonitoringForCompany;
+
+export const syncStrategyToGraph = base44.functions.syncStrategyToGraph;
+
+export const exportTSIReport = base44.functions.exportTSIReport;
+
+export const deleteConversation = base44.functions.deleteConversation;
+
+export const indexKnowledgeSource = base44.functions.indexKnowledgeSource;
+
+export const searchKnowledge = base44.functions.searchKnowledge;
+
+export const checkPermission = base44.functions.checkPermission;
+
+export const initializeDefaultRoles = base44.functions.initializeDefaultRoles;
+
+export const shareEntity = base44.functions.shareEntity;
+
+export const claudeAnalysis = base44.functions.claudeAnalysis;
+
